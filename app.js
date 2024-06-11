a58a26760bc46c7cd9c113b778ead601bbed861a
theConnectorr <kylekhoitv@gmail.com>
2024-06-11 20:47:36 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,20 +1,130 @@
-let stars = document.getElementById('stars');
-let moon = document.getElementById('moon');
-let mountains_behind = document.getElementById('mountains_behind');
-let text = document.getElementById('text');
-let btn = document.getElementById('btn');
-let mountains_front = document.getElementById('mountains_front');
-let header = document.querySelector('header');
 
-window.addEventListener('scroll', function () {
-    let value = window.scrollY;
-    stars.style.top = -value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
-    moon.style.right = 100 * Math.sin(0.02 * value) + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
-    mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
-    header.style.top = value * 0.5 + 'px';
-})
+const li1 = document.getElementById("li1");
+const btn1 = document.getElementById("btn1");
+const shower = document.getElementById("shower");
+const listId = "AQMkADAwATMwMAItMWYxNC04Y2U0LTAwAi0wMAoALgAAA8uNLzcGhbdHjhnMKBY42HoBALWCHmmTzCdFi8NoB6pstOEABKXjwZ4AAAA=";
+
+const words = [
+    {
+        "word": "hello",
+        "partOfSpeech": "verb",
+        "definition": "To greet"
+    },
+    {
+        "word": "registration",
+        "partOfSpeech": "noun",
+        "definition": "The act of signing up or registering for something."
+    }
+]
+
+function add(word, text) {
+    if (word.children.length === 0) {
+        const newNode = document.createElement("pre");
+        newNode.textContent = text;
+
+        word.appendChild(newNode);
+    } else {
+        word.removeChild(word.lastChild);
+    }
+}
+
+function shortPOS(pos) {
+    if ("noun" === pos)
+        return "n";
+    if (pos === "adjective")
+        return "adj";
+    if (pos === "verb")
+        return "v";
+}
+
+for (let i in words) {
+    const newListItem = document.createElement("li");
+    newListItem.classList.add("word");
+    const content = `${words[i]["word"]} (${shortPOS(words[i]["partOfSpeech"])})`;
+    newListItem.textContent = content;
+
+    const body = words[i]["definition"];
+    newListItem.addEventListener("click", (event) => {
+        add(event.currentTarget, body);
+    });
+
+    shower.appendChild(newListItem);
+}
+
+// async function main() {
+//     let client_id = "haahs";
+//     let client_secret = "ahhah";
+//     let refresh_token = "ahheh";
+//     const grant_type = "refresh_token";
+//     const scope = "Tasks.ReadWrite";
+//     let access_token = "Tasks.ReadWrite";
+
+//     await fetch("./json/vole_phi.json")
+//         .then((response) => response.json())
+//         .then((json) => {
+//             client_id = json["client_id"];
+//             client_secret = json["client_secret"];
+//             refresh_token = json["refresh_token"];
+//         });
+
+//     var formBody = [
+//         `${encodeURIComponent("client_id")}=${encodeURIComponent(client_id)}`,
+//         `${encodeURIComponent("client_secret")}=${encodeURIComponent(client_secret)}`,
+//         `${encodeURIComponent("refresh_token")}=${encodeURIComponent(refresh_token)}`,
+//         `${encodeURIComponent("grant_type")}=${encodeURIComponent(grant_type)}`,
+//         `${encodeURIComponent("scope")}=${encodeURIComponent(scope)}`
+//     ];
+//     formBody = formBody.join("&");
+
+//     console.log(formBody);
+
+//     await fetch("https://login.microsoftonline.com/common/oauth2/v2.0/token", {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/x-www-form-urlencoded;charset=UTF-8"
+//         },
+//         body: formBody
+//     }).then((response) => {
+//         console.log(response);
+//         if (!response.ok) {
+//             console.log('dumaloiroi');
+//         }
+//         return response.json();
+//     }).then((json) => {
+//         console.log(json);
+//         access_token = json["access_token"];
+//         console.log(access_token);
+//     })
+// }
+
+// main()
+
+async function main() {
+    const config = {
+        auth: {
+            clientId: "fb00ce00-b4fb-4902-8276-10ba22372a0d",
+            authority: "https://login.microsoftonline.com/common/",
+            redirectUri: "http://localhost:5500/index.html"
+        },
+    };
+
+    var client = new Msal.UserAgentApplication(config);
+    var request = {
+        scopes: ['tasks.readwrite']
+    }
+
+    let loginResponse = await client.loginPopup(request);
+    console.log(loginResponse);
+    let tokenResponse = await client.acquireTokenSilent(request);
+    console.dir(tokenResponse);
+
+    let payload = await fetch("https://graph.microsoft.com/v1.0/me/todo/lists", {
+        headers: {
+            "Authorization": "Bearer " + tokenResponse.accessToken
+        }
+    })
+    let result = await payload.json();
+    console.log(result);
+}
+
+main();
