8dcd73730e3210c478f2dba3524361e8acf88dd7
Vo Tran Phi <songokupkj@gmail.com>
2024-06-30 18:06:33 +0700

    feat: scramble and its image

--- RubiksTimor/app.js
+++ RubiksTimor/app.js
@@ -1,6 +1,50 @@
+/* VARIABLES INITIALIZATION */
+
 const timer = document.getElementById("timer");
 const monitor = document.getElementById("monitor");
 const list = document.getElementById("solve-list");
+const scramble = document.getElementById("scramble");
+const scrambleImage = document.getElementById("scramble-img");
+
+var cstimerWorker = (function() {
+	var worker = new Worker('/node_modules/cstimer_module/cstimer_module.js');
+
+	var callbacks = {};
+	var msgid = 0;
+
+	worker.onmessage = function(e) {
+		var data = e.data; //data: [msgid, type, ret]
+		var callback = callbacks[data[0]];
+		delete callbacks[data[0]];
+		callback && callback(data[2]);
+	}
+
+	function callWorkerAsync(type, details) {
+		return new Promise(function(type, details, resolve) {
+			++msgid;
+			callbacks[msgid] = resolve;
+			worker.postMessage([msgid, type, details]);
+		}.bind(null, type, details));
+	}
+
+	return {
+		getScrambleTypes: function() {
+			return callWorkerAsync('scrtype');
+		},
+		getScramble: function() {
+			return callWorkerAsync('scramble', Array.prototype.slice.apply(arguments));
+		},
+		setSeed: function(seed) {
+			return callWorkerAsync('seed', [seed]);
+		},
+		setGlobal: function(key, value) {
+			return callWorkerAsync('set', [key, value]);
+		},
+		getImage: function(scramble, type) {
+			return callWorkerAsync('image', [scramble, type]);
+		}
+	}
+})();
 
 let isInspecting = false;
 let startInspect;
@@ -11,6 +55,8 @@ let startHold;
 let isTiming = false;
 let startTime;
 
+let scrambleStr;
+
 let timingId;
 let holdingId;
 const HOLDING_TIME = 500;
@@ -20,11 +66,17 @@ const UI_UPDATING_RATE = 10;
 
 let solveList = [];
 
+/* LOAD EMELEMTS FOR THE FIRST TIME */
+
 if (!localStorage.getItem("333")) {
     localStorage.setItem("333", "[]");
 } else {
     loadSolveFromLocal();
 }
+
+loadScrambleAndImage();
+
+/* FUNCTIONS */
 
 function loadSolveFromLocal() {
     solveList = JSON.parse(localStorage.getItem("333"));
@@ -58,13 +110,16 @@ function timeStopHandler() {
     const time = stopTime - startTime;
     
     timer.innerHTML = displayTime(time);
-
+    
     const solve = {
         "id": `${startTime}`,
         "time": `${time}`,
-        "scramble": "haha no scramble yet",
+        "scramble": scrambleStr,
         "penalty": "None"
     }
+
+    loadScrambleAndImage();
+    setScrambleAndImageVisibility(true);
     
     solveList.push(solve);
     pushSolveToList(solveList.length, solve);
@@ -94,6 +149,8 @@ function holdingStopHandler() {
         startTime = new Date().getTime();
         isTiming = true;
         timingId = setInterval(timingUpdate, UI_UPDATING_RATE);
+
+        setScrambleAndImageVisibility(false);
     }
     isHolding = false;
 }
@@ -120,6 +177,30 @@ function displayTime(ms) {
     return `${(ms / 1000).toFixed(3)}`
 }
 
+function clearList() {
+    localStorage.clear();
+    location.reload();
+}
+
+function loadScrambleAndImage() {
+    var queue = cstimerWorker.getScramble('333').then(function(scrStr) {
+        scrambleStr = scrStr;
+        scramble.innerText = scrStr;
+        return cstimerWorker.getImage(scrStr, '333');
+    }).then(function(svgImage) {
+        scrambleImage.innerHTML = svgImage;
+    });
+}
+
+function setScrambleAndImageVisibility(isVisible) {
+    var visibility = isVisible ? "block" : "none";
+
+    scramble.style.display = visibility;
+    scrambleImage.style.display = visibility;
+}
+
+/* EVENT LISTENERS */
+
 window.addEventListener("keydown", (e) => {
     // monitor.innerHTML = `Down: key = ${e.key}, code = ${e.code}, ${new Date().getTime() - startHold}`;
     if (!isHolding) {
@@ -140,17 +221,15 @@ window.addEventListener("keyup", (e) => {
     }
 });
 
-function clearList() {
-    localStorage.clear();
-    location.reload();
-}
-
 /*
-    - Holding effect for the timer 
-    - Implement the inspection rule below
+    - Holding effect for the timer -ok
     - Number format for the timer -ok
     - Save the time using localStorage -ok
     - Implement the settings feature for each local user
+
+    - Load the scramble and scramble image -ok
+    - Refresh scramble after every solution -ok
+    - Hide the scramble and image when timer is running -ok
 
     note: The competitor must start the solve within 15 seconds of the start of the inspection. Penalty: time penalty (+2 seconds).
     note: The competitor must start the solve within 17 seconds of the start of the inspection. Penalty: disqualification of the attempt (DNF).
